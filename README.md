8cd86892cfeb467bf3aff9a203c939a8f72c59dd
David Aronsohn <hipster@slickdeals.net>
2015-05-29 18:54:37 -0700

    Inital Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chef server setup
+
+This is just a quick and dirty script and repo for bootstrapping my, or maybe your chef server.
+
+Most of this is directly from the [chef-server cookboks's readme](https://github.com/chef-cookbooks/chef-server)
+
+## Usage
+1. Fork
+2. Edit server.json
+3. donwload onto new server and set the hostname
+4. run chef_run
+
+## Assumptions
+* git is installed
+* curl is installed
+* The user running this has sudo
+* You are running this on a chef-supported lunix platform
---
+++ chef_run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "Pull latest git to be sure"
+git pull origin master || exit 5
+
+echo "Ensure we have the right chef"
+curl -L https://www.chef.io/chef/install.sh | sudo bash -s -- -v 12.3.0
+
+echo "Make chef working directories"
+sudo mkdir -p /var/chef/cache /var/chef/cookbooks
+
+echo "Install pre-reqs"
+for i in "chef-server" "chef-server-ingredient" "packagecloud"; do
+  curl -#L https://supermarket.chef.io/cookbooks/$i/download | sudo tar xvzC /var/chef/cookbooks
+done
+
+sudo chef-client -z --runlist 'recipe[chef-server::default]' -j server.json
