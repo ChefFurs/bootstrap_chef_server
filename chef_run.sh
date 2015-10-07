b7641342944db4c96d122a853a80f2f02f25099f
David Aronsohn <WagThatTail@Me.com>
2015-10-06 17:24:58 -0700

    Move to pulling the new chef-ingredient

--- chef_run.sh
+++ chef_run.sh
@@ -8,7 +8,7 @@ echo "Make chef working directories"
 mkdir cookbooks
 
 echo "Install pre-reqs"
-for i in "chef-server" "chef-server-ingredient" "packagecloud"; do
+for i in "chef-server" "chef-ingredient" "packagecloud"; do
   curl -#L https://supermarket.chef.io/cookbooks/$i/download | sudo tar xvzC cookbooks/
 done
 
